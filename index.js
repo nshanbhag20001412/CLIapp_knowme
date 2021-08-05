e827658b16c4e1b085b5df39a5147578cee5e450
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-08-05 15:51:14 +0530

    created

---
+++ index.js
@@ -0,0 +1,73 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+// data of high score
+var highScores = [
+  {
+    name: "Neha",
+    score: 3,
+  },
+
+  {
+    name: "Abhay",
+    score: 2,
+  },
+]
+
+// array of objects
+var questions = [{
+  question: "Where do I live? ",
+  answer: "Pune"
+}, {
+  question: "My favorite food is? ",
+  answer: "Pizza"
+},
+{
+  question: "Whats my instagram id? ",
+  answer: "__boomchikiwawa____"
+}];
+
+function welcome() {
+ var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome "+ userName + " to how much do you know me? ft:Neha Shanbhag");
+}
+
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+    
+  } else {
+    console.log("wrong!");
+   
+  }
+
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i=0; i<questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcome();
+game();
+showScores();
